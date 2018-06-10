dd7d99453fdea488cbf2b7ced596840ce04d7562
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-06-10 19:22:36 +0200

    feat: updated Header & Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,53 @@ const FooterContainer = styled.div`
   width: 100%;
   margin-top: auto;
   border-top: 2px solid lightgray;
+  padding: 2rem 0 1rem 0;
 `
 const Footer = styled.footer`
   min-height: 5rem;
   display: flex;
   align-items: flex-end;
-  justify-content: flex-end;
+  justify-content: space-between;
 `
-
+const ImprintContainer = styled.div`
+  > p {
+    margin: 0.2rem 0;
+  }
+`
+const SocialContainer = styled.div`
+  display: flex;
+  align-self: flex-start;
+  > p {
+    margin: 0 0.5rem;
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: grey;
+    color: #fff;
+    font-size: 1.2rem;
+    font-weight: 600;
+  }
+`
 export default () => (
   <FooterContainer>
     <Footer className="max-width">
-      <p style={{ marginRight: '1rem' }}>Impressum</p>
-      <p>Datenschutz</p>
+      <ImprintContainer>
+        <p>Geschäftskunden</p>
+        <p>Kontakt</p>
+        <p>FAQ</p>
+        <p>Sicherheitshinweise</p>
+        <p>AGB</p>
+        <p>Datenschutz</p>
+        <p>Impressum</p>
+      </ImprintContainer>
+      <SocialContainer>
+        <p style={{ background: '#4C00FF' }}>f</p>
+        <p style={{ background: '#FF0000' }}>G+</p>
+        <p style={{ background: '#649AEB' }}>🕊</p>
+      </SocialContainer>
     </Footer>
   </FooterContainer>
 )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import logo from '../assets/icons/drone.svg'
+import search from '../assets/icons/search.svg'
+
 const HeaderContainer = styled.div`
   position: fixed;
   top: 0;
@@ -21,16 +23,32 @@ const Header = styled.header`
 const Logo = styled.img`
   width: 3rem;
 `
-
+const HeaderPartial = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  > * {
+    margin: 0 1rem;
+  }
+`
 export default () => (
   <HeaderContainer>
     <Header className="max-width">
-      <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
-        <Logo src={logo} alt="Logo, drone carrying parcel" />airlivery
-      </Link>
-      <Link to="/senden">Senden</Link>
-      <Link to="/empfangen">Empfangen</Link>
-      <Link to="/about">About</Link>
+      <HeaderPartial>
+        <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+          <Logo src={logo} alt="Logo, drone carrying parcel" />airlivery
+        </Link>
+        <Link to="/senden">Senden</Link>
+        <Link to="/empfangen">Empfangen</Link>
+        <Link to="/about">About</Link>
+      </HeaderPartial>
+      <HeaderPartial style={{ justifyContent: 'flex-end' }}>
+        <a href="/#">Login/Registrieren</a>
+        <a href="/#">Hilfe</a>
+        <a href="/#">
+          <Logo src={search} alt="Suche" style={{ width: '1.5rem' }} />
+        </a>
+      </HeaderPartial>
     </Header>
   </HeaderContainer>
 )
