12bdd80dd8c92d206e7f916d1b845f6b7397d569
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2021-12-03 05:25:44 +0100

    feat: add full caching support

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,12 @@ import { clientsClaim } from 'workbox-core'
 import { ExpirationPlugin } from 'workbox-expiration'
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
-import { StaleWhileRevalidate } from 'workbox-strategies'
+import { CacheableResponsePlugin } from 'workbox-cacheable-response'
+import {
+  CacheFirst,
+  StaleWhileRevalidate,
+  NetworkFirst,
+} from 'workbox-strategies'
 
 clientsClaim()
 
@@ -50,15 +55,48 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) =>
-    url.origin === self.location.origin &&
-    url.pathname.match(/\.(png|svg|jpe?g)$/), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url, request }) =>
+    url.origin === self.location.origin && request.destination === 'image', // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
       new ExpirationPlugin({ maxEntries: 50 }),
+    ],
+  }),
+)
+
+registerRoute(
+  ({ url }) =>
+    url.origin === 'https://randomuser.me' && url.pathname === '/api/',
+  new NetworkFirst({
+    cacheName: 'user-api',
+    networkTimeoutSeconds: 5,
+  }),
+)
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.googleapis.com',
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  }),
+)
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.gstatic.com',
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
     ],
   }),
 )
